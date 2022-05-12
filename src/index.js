fccda671b38b77d557eabf66470c063d94c75411
Selin Su Varol <selinsuvarol@gmail.com>
2022-05-12 15:14:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  console.log(taskForm)
+
+  let taskList = document.querySelector("#tasks")
+  console.log(taskList)
+
+  taskForm.addEventListener("submit",clickHandler)
+
 });
+
+ function clickHandler(event){
+  event.preventDefault();
+
+  const taskDescription = document.getElementById('new-task-description').value;
+  console.log(taskDescription)
+  let listItem= document.createElement("li")
+  listItem.innerText = taskDescription
+  document.querySelector("#tasks").appendChild(listItem);
+
+  const but = document.createElement("button")
+  but.innerText = "X"
+  listItem.appendChild(but)
+  but.addEventListener("click", deleteHandler)
+
+ }
+
+ function deleteHandler (event){
+  event.preventDefault()
+  //event.target.closest("li").remove()
+  //this.parentNode.remove()
+  this.parentElement.remove()
+
+ }
